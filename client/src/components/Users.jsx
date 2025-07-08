b647be91490503b6e1fd7c0853d4817877d4b9ab
AtharvSawant-15 <atharvsawant.business@gmail.com>
2025-07-09 02:17:31 +0530

    frontend is done just css styling is required and server side setup as done

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from "react-router-dom";
 
 const Users = () => {
+  const [users, setUsers] = useState([{
+    Name: "Atharv", Email: "atharv@gamil.com", Age: 20
+  }])
   return (
-    <div>Users</div>
+    <div className='d-flex bg-primary justify-content-center algin-items-center py-5'>
+      <div className='w-50 bg-white rounded p-3'>
+        <Link to="/create" className='btn btn-success'>Add +</Link>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Age</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.map((users) => {
+                return <tr>
+                  <td>{users.Name}</td>
+                  <td>{users.Email}</td>
+                  <td>{users.Age}</td>
+                  <td>
+                    <Link to="/update" className='btn btn-success'>Edit</Link>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              })
+            }
+
+
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
